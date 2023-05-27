e33bad6959d0a4eaf2ec48c93f5f7b3ad0bafcc3
Tatiana E <122121318+thatdevx@users.noreply.github.com>
2023-05-27 13:20:43 -0300

    atualizando

---
+++ Aula_01-27-04-23/aula_01_270423.php
@@ -0,0 +1,9 @@
+<?php
+
+for ($cont=1; $cont<=10; $cont++){
+	for($kont=1; $kont<=10; $kont++){
+		echo $cont*$kont ."\t";
+		}
+		echo "<br>";
+
+}
---
+++ Aula_02-04-05-23/aula04_05_23.php
@@ -0,0 +1,27 @@
+<?php
+
+$salarioMinimo = $_POST['salarioMinimo'];
+$horas = $_POST['horas'];
+$dependentes = $_POST['dependentes'];
+
+if($salarioMinimo <=0 || $horas <=0 or $dependentes <= 0){
+
+    echo "Valor invalido. Informe todos os valores corretamente.";
+
+    }else {
+
+        $valorHora=$salarioMinimo/5;
+        $bruto=$horas*$valorHora;
+        $imposto=$bruto*0.059;
+        $ValorPorDependentes=($bruto/12)*$dependentes;
+        $salariofinal=$bruto - $imposto + $ValorPorDependentes;
+
+        $salario=number_format($salariofinal, 2, ',', '.');
+
+        echo "
+                O valor de sua hora é de R$ $valorHora. <br>
+                Seu imposto equivale à $imposto <br>
+                E o valor que você tem a receber por dependentes é de R$ $ValorPorDependentes <br>
+                O salario total á receber é R$ $salario";
+
+    }
